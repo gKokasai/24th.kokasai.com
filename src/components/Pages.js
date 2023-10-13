53bbc1425458b2d874e66512b630fa799bb6bad5
m0mizuki <mutou.m.2020@gmail.com>
2023-10-13 19:47:54 +0900

    change: 24th.kokasai.comのドメインで動作するよう修正

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,39 +1,39 @@
 export const Pages={
     access:{
         displayName:"アクセス",
-        path:"/24th.kokasai.com/access",
+        path:"/access",
         icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/accessIcon.png`,
     },
     event:{
         displayName:"ステージ企画",
-        path:"/24th.kokasai.com/event",
+        path:"/event",
         icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/eventIcon.png`,
     },
     home:{
         displayName:"ホーム",
-        path:"/24th.kokasai.com",
+        path:"/",
         icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/homeIcon.png`,
     },
     map:{
         displayName:"マップ",
-        path:"/24th.kokasai.com/map",
+        path:"/map",
         icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/mapIcon.png`,
     },
     project:{
         displayName:"企画",
-        path:"/24th.kokasai.com/project",
+        path:"/project",
         icon:`${process.env.PUBLIC_URL}/img/menuBarIcon/projectIcon.png`,
     },
     credit:{
         displayName:"クレジット",
-        path:"/24th.kokasai.com/credit",
+        path:"/credit",
     },
     apologize:{
         displayName:"作成中",
-        path:"/24th.kokasai.com/apologize",
+        path:"/apologize",
     },
     projectDetail:{
         displayName:"企画詳細",
-        path:"/24th.kokasai.com/projectDetail",
+        path:"/projectDetail",
     }
 };
