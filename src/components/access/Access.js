eb7efa311585d95c8dffe4e5bcd97930b66870a6
m0mizuki <mutou.m.2020@gmail.com>
2023-10-29 17:55:50 +0900

    feat: アクセスに駐車場についての説明を追加

--- src/components/access/Access.js
+++ src/components/access/Access.js
@@ -32,18 +32,18 @@ function Access() {
         let fish = document.createElement("img");
         fish.classList.add("fish");
 
-        let random = Math.floor( Math.random()*3); //0~2
-        if(random==0){
-          fish.src=`${process.env.PUBLIC_URL}/img/utility/fish1.png`;
-          fish.style.transform="scale(-1,1) rotate(45deg)";
+        let random = Math.floor(Math.random() * 3); //0~2
+        if (random == 0) {
+          fish.src = `${process.env.PUBLIC_URL}/img/utility/fish1.png`;
+          fish.style.transform = "scale(-1,1) rotate(45deg)";
         }
-        else if(random==1){
-          fish.src=`${process.env.PUBLIC_URL}/img/utility/fish2.png`;
-          fish.style.transform="scale(-1,1) rotate(45deg)";
+        else if (random == 1) {
+          fish.src = `${process.env.PUBLIC_URL}/img/utility/fish2.png`;
+          fish.style.transform = "scale(-1,1) rotate(45deg)";
         }
-        else if(random==2){
-          fish.src=`${process.env.PUBLIC_URL}/img/utility/fish3.png`;
-          fish.style.transform="scale(-1,1) rotate(0deg)";
+        else if (random == 2) {
+          fish.src = `${process.env.PUBLIC_URL}/img/utility/fish3.png`;
+          fish.style.transform = "scale(-1,1) rotate(0deg)";
         }
 
         //fish.style.animationDelay = i * 0.2 + Math.random() * 0.2 - 0.1 + "s";
@@ -57,7 +57,7 @@ function Access() {
 
     //fishAreaに当たり判定吸われるので非表示にする
     setTimeout(() => {
-      fishArea.style.display="none";
+      fishArea.style.display = "none";
     }, 2700); //アニメーション時間+delayの最大
   }, []);
 
@@ -71,6 +71,9 @@ function Access() {
           <p className="heading2">周辺地図</p>
           <iframe id="googleMap" className="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3212.3668851173343!2d139.02025627574992!3d36.37611147237107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601e8cc32e0daf8d%3A0x207d7e6356e87ed8!2z576k6aas5bel5qWt6auY562J5bCC6ZaA5a2m5qCh!5e0!3m2!1sja!2sjp!4v1691332826280!5m2!1sja!2sjp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
+          <br />
+          <br />
+
           <p className="heading2">アクセス</p>
 
           <AccessContent>
@@ -80,7 +83,6 @@ function Access() {
               <li>タクシー約10分</li>
             </ul>
           </AccessContent>
-          <br />
 
           <AccessContent>
             <p className="heading3">JR井野駅より</p>
@@ -95,7 +97,6 @@ function Access() {
               </li>
             </ul>
           </AccessContent>
-          <br />
 
           <AccessContent>
             <p className="heading3">JR高崎駅より</p>
@@ -111,12 +112,35 @@ function Access() {
               <li>タクシー約30分</li>
             </ul>
           </AccessContent>
-          <br />
 
           <AccessContent>
             <p className="heading3">関越自動車道 前橋インターチェンジより（約1km）</p>
             <ul className="ul_access">
               <li>車で約5分</li>
+            </ul>
+          </AccessContent>
+
+          <br />
+
+          <p className="heading2">駐車場・駐輪場について</p>
+
+          <AccessContent>
+            <ul className="ul_access">
+              <li>乗用車でお越しの方は、南西にある<span className="heading4">大駐車場</span>もしくは<span className="heading4">南門から入って右手の駐車場</span>をご利用ください</li>
+            </ul>
+            <p>※駐車場は両日<span className="heading3">9:30</span>からの解放となります</p>
+            <p>※乗用車は一家族様につき一台での来場をお願いします</p>
+          </AccessContent>
+
+          <AccessContent>
+            <ul className="ul_access">
+              <li>原付・バイクでお越しの方は、<span className="heading4">大駐車場の駐輪スペースの東側半分</span>をご利用ください</li>
+            </ul>
+          </AccessContent>
+
+          <AccessContent>
+            <ul className="ul_access">
+              <li>自転車でお越しの方は、<span className="heading4">正門から入って右奥の駐輪場</span>をご利用ください</li>
             </ul>
           </AccessContent>
 
