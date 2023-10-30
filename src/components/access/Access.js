f89372e4ee3fd7f4979a506dbcc1c0a80d764de9
m0mizuki <mutou.m.2020@gmail.com>
2023-10-30 22:04:27 +0900

    change: googleマップをカスタムのものに変更

--- src/components/access/Access.js
+++ src/components/access/Access.js
@@ -73,7 +73,7 @@ function Access() {
         <div className="contents_innerBlock">
           <br />
           <p className="heading2">周辺地図</p>
-          <iframe id="googleMap" className="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3212.3668851173343!2d139.02025627574992!3d36.37611147237107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601e8cc32e0daf8d%3A0x207d7e6356e87ed8!2z576k6aas5bel5qWt6auY562J5bCC6ZaA5a2m5qCh!5e0!3m2!1sja!2sjp!4v1691332826280!5m2!1sja!2sjp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe id="googleMap" className="googleMap" src="https://www.google.com/maps/d/embed?mid=1yr0FgpbJGUNZzaKegu56loIZQUq-Klo&ehbc=2E312F"></iframe>
 
           <br />
           <br />
